cb65f2d51931eb2d1104627f7333fcfafcc5dd37
Sergey Bykov <derter@kitendo.ru>
2021-07-21 13:09:25 +0500

    pi.py

--- pi.py
+++ pi.py
@@ -1,13 +1,20 @@
+#!/home/pi/ticketook/backend/env/bin/python
+
 import os
 from gpiozero import Button
 from time import sleep
 import requests
 from PIL import Image, ImageDraw, ImageFont
 
-API_URL = 'http://192.168.0.48:8000/api/'
+
+API_URL = 'http://local.ticketook.ru/api/'
 ACCESS_KEY = '123'
+B1_PORT = 2
+B2_PORT = 3
+
 
 def print_check(msg):
+    print('creating image')
     W, H = (600,400)
     im = Image.new("RGBA",(W,H),"white")
     draw = ImageDraw.Draw(im)
@@ -19,15 +26,24 @@ def print_check(msg):
     w, h = draw.textsize(msg, font=myFont)
     draw.text(((W-w)/2,(H-h)/2), msg, fill="black", font=myFont)
     im.save("check.png", "PNG")
+    print('printing')
     os.system('lp check.png')
+    print('done')
 
+active_session_id = None
 
-r = requests.get(API_URL + 'zone/1/info/')
-zone_info = r.json()
-active_session_id = zone_info['active_session_id']
+while active_session_id is None:
+    try:
+        r = requests.get(API_URL + 'zone/1/info/')
+        zone_info = r.json()
+        active_session_id = zone_info['active_session_id']
+    except:
+        sleep(5)
 
-button_1 = Button(1)
-button_2 = Button(2)
+print('Active session id:', active_session_id)
+
+button_1 = Button(B1_PORT)
+button_2 = Button(B2_PORT)
 
 button_1_state = False
 button_2_state = False
@@ -36,38 +52,41 @@ counter = 0
 
 while True:
 
-    print('tick')
-
     if button_1.is_pressed and button_1_state == False:
         print('B1 pressed!')
         try:
-            r = requests.post(API_URL + 'ticket/', data={ "access_key": ACCESS_KEY, "service_slug": "first", "session_id": active_session_id })
+            r = requests.post(API_URL + 'ticket/', json={ "access_key": ACCESS_KEY, "service_slug": "first", "session_id": active_session_id })
             print_check(r.json()['ticket']['full_number'])
+            sleep(3)
         except:
             pass
-        sleep(3)
         button_1_state = True
-    else:
+    if not button_1.is_pressed and button_1_state == True:
         button_1_state = False
-    
+
     if button_2.is_pressed and button_2_state == False:
         print('B2 pressed!')
         try:
-            r = requests.post(API_URL + 'ticket/', data={ "access_key": ACCESS_KEY, "service_slug": "second", "session_id": active_session_id })
+            r = requests.post(API_URL + 'ticket/', json={ "access_key": ACCESS_KEY, "service_slug": "second", "session_id": active_session_id })
             print_check(r.json()['ticket']['full_number'])
+            sleep(3)
         except:
             pass
-        sleep(3)
         button_2_state = True
-    else:
+    if not button_2.is_pressed and button_2_state == True:
         button_2_state = False
+
 
     counter += 1
 
-    if counter == 15:
-        r = requests.get(API_URL + 'zone/1/info/')
-        zone_info = r.json()
-        active_session_id = zone_info['active_session_id']
+    if counter == 300:
+        try:
+            r = requests.get(API_URL + 'zone/1/info/')
+            zone_info = r.json()
+            active_session_id = zone_info['active_session_id']
+        except:
+            pass
         counter = 0
 
-    sleep(1)
+    sleep(0.05)
+
